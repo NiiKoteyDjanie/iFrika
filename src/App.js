0824a9934e9be1f952405cf3f9cade11c93a18a4
NiiKoteyDjanie <sampson.kotey.djanie@gmail.com>
2022-12-24 21:13:55 +0000

    add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import React from 'react';
 function App() {
   return (
-    <div >
-      <header >
-        <a href="/">iFrika</a>
+    <BrowserRouter>
+    <div>
+      <header>
+        <Link to="/">iFrika</Link>
       </header>
-      <main>list product</main>
+      <main>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+        </Routes>
+      </main>
     </div>
-  );
+  </BrowserRouter>
+);
 }
 
 export default App;
